9be80fa664a761403be7ee2956a6b0aa04146617
Richard Wrench <rwrench523@gmail.com>
2025-05-19 12:01:10 -0500

    added ytd %

--- PythonApp1.py
+++ PythonApp1.py
@@ -22,8 +22,7 @@ for ticker in tickers:
             ytd_open = data['Close'].iloc[0]
             latest_close = data['Close'].iloc[-1]
             ytd_pct_change = ((latest_close - ytd_open) / ytd_open) * 100
-            print(f"{ticker:<6} {latest_close.item():>10.2f}")
-    #{ytd_pct_change:>14.2f}%")
+            print(f"{ticker:<6} {latest_close.item():>10.2f} {ytd_pct_change.item():>14.2f}%")
        else:
             print(f"{ticker:<6} {'N/A':>10} {'N/A':>15}")
    except Exception as e:
