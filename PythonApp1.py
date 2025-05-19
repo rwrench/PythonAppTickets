5e7d64a5aabf178f50dea30314af57eef53a0012
Richard Wrench <rwrench523@gmail.com>
2025-05-19 11:55:55 -0500

    Added exception handling, corrected output for latest close

--- PythonApp1.py
+++ PythonApp1.py
@@ -9,17 +9,22 @@ qtys = [100, 400, 97]
 today = datetime.today().strftime('%Y-%m-%d')
 start_of_year = f'{datetime.today().year}-01-01'
 
-print("{'Ticker':<6} {'Close':>10} {'YTD % Change':>15}")
+print('Ticker Close YTD % Change')
 print("-" * 35)
 
 for ticker in tickers:
-    data = yf.download(ticker, start=start_of_year, end=today)
-    if not data.empty:
-        close = data['Close']
-        ytd_open = data['Close'].iloc[0]
-        latest_close = data['Close'].iloc[-1]
-        ytd_pct_change = ((latest_close - ytd_open) / ytd_open) * 100
-        print(f"{ticker:<6} {latest_close:>10.2f} {ytd_pct_change:>14.2f}%")
-    else:
-        print(f"{ticker:<6} {'N/A':>10} {'N/A':>15}")
-
+   try:
+       data = yf.download(ticker, 
+             start=start_of_year, 
+             end=today,progress=False)
+       if not data.empty:
+            close = data['Close']
+            ytd_open = data['Close'].iloc[0]
+            latest_close = data['Close'].iloc[-1]
+            ytd_pct_change = ((latest_close - ytd_open) / ytd_open) * 100
+            print(f"{ticker:<6} {latest_close.item():>10.2f}")
+    #{ytd_pct_change:>14.2f}%")
+       else:
+            print(f"{ticker:<6} {'N/A':>10} {'N/A':>15}")
+   except Exception as e:
+           print(f"{ticker:<6} {'ERROR':>10} {'N/A':>15}  ({e})")
